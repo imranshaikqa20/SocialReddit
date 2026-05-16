0a62ed50d9821fca298e50277dbfe8562686a636
Shaik imran <imranshaikqa20@gmail.com>
2026-05-16 23:45:09 +0530

    Updated frontend API URL

--- frontend/reddit-frontend-starter/src/services/api.js
+++ frontend/reddit-frontend-starter/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://socialreddit-backend.onrender.com/api",
 
   headers: {
     "Content-Type": "application/json"
