6a39af14a6bf6b4563f0061adbbf8a01c53f2e2a
Shaik imran <imranshaikqa20@gmail.com>
2026-05-18 13:57:11 +0530

    Fixed delete post functionality in PostCard

--- backend/reddit-backend-starter/src/main/java/com/socialmedia/backend/repository/VoteRepository.java
+++ backend/reddit-backend-starter/src/main/java/com/socialmedia/backend/repository/VoteRepository.java
@@ -3,7 +3,13 @@ package com.socialmedia.backend.repository;
 import com.socialmedia.backend.entity.Post;
 import com.socialmedia.backend.entity.Vote;
 
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.repository.query.Param;
 
 import org.springframework.stereotype.Repository;
 
@@ -41,8 +47,19 @@ public interface VoteRepository
        DELETE ALL VOTES OF POST
     ========================================= */
 
-    void deleteByPost(
-            Post post
+    @Transactional
+
+    @Modifying
+
+    @Query(
+            "DELETE FROM Vote v WHERE v.post.id = :postId"
+    )
+
+    void deleteVotesByPostId(
+
+            @Param("postId")
+            Long postId
+
     );
 
 }
